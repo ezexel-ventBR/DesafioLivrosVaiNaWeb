2a7403d94350adc9b55ed4251fb1a74d3d254268
ezexel-ventBR <ezequielfigsilva22@gmail.com>
2025-03-28 15:52:45 -0300

    conectando API livros na rota /doar para receber os valores escritos em Quero Doar

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,30 +1,63 @@
 import vector from "../../assets/vector.png"
 import s from "./queroDoar.module.scss"
+import { useState } from "react"
+import axios from "axios"
+
 export default function QueroDoar() {
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [image_url, setImage_url] = useState("")
+
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value)
+    }
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+    const capturaImage_url = (e) => {
+        setImage_url(e.target.value)
+    }
+
+    const enviarDados = async() => {
+        const dadosPEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+
+        await axios.post("https://python-apilivros-vnw.onrender.com/doar", dadosPEnviar)
+    }
+
     return (
         <section class={s.queroDoar}>
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-            <form className={s.formDoar}>
+            <form className={s.formDoar} onSubmit={(e) => e.preventDefault()}>
                 <section className={s.tituloForm}>
                     <img src={vector} alt="imagem de um livro azul com interior transparente" />
                     <p>Informações do Livro</p>
                 </section>
                 <section className={s.informacoesDoador}>
                     <section className={s.informacao}>
-                        <input type="text" placeholder="Titulo"/>
+                        <input type="text" placeholder="Titulo" onChange={capturaTitulo}/>
                     </section>
                     <section className={s.informacao}>
-                        <input type="text" placeholder="Categoria"/>
+                        <input type="text" placeholder="Categoria" onChange={capturaCategoria}/>
                     </section>
                     <section className={s.informacao}>
-                        <input type="text" placeholder="Autor"/>
+                        <input type="text" placeholder="Autor" onChange={capturaAutor}/>
                     </section>
                     <section className={s.informacao}>
-                        <input type="text" placeholder="Link da Imagem"/>
+                        <input type="text" placeholder="Link da Imagem" onChange={capturaImage_url}/>
                     </section>
                 </section>
                 <section className={s.botaoDoar}>
-                        <button>Doar</button>
+                        <button onClick={enviarDados}>Doar</button>
                 </section>
             </form>
         </section>
